79eac20a85b0af703fd7c6dca8ec1dbb58240bcd
TuckerFaulk <faulkner1993@hotmail.co.uk>
2022-12-13 15:00:40 +0000

    Add view and html for equipment list page and link url

--- calculator/urls.py
+++ calculator/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Index, name='index'),
     path('calculator/', views.CalculatorListView.as_view(), name='calculator_list'),
+    path('equipment/', views.EquipmentListView.as_view(), name='equipment_list'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -7,7 +7,18 @@ def Index(request):
     return render(request, 'index.html', {})
 
 
+# Calculator Views
+
+
 class CalculatorListView(generic.ListView):
     model = Calculator
     queryset = Calculator.objects.order_by('make_and_model')
     template_name = 'calculator.html'
+
+
+# Equipment Views
+
+class EquipmentListView(generic.ListView):
+    model = Equipment
+    queryset = Equipment.objects.filter(approved=True)
+    template_name = 'equipment.html'
