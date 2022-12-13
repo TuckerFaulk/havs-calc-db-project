83535c515d253087a79426349c13cb0205565e2c
TuckerFaulk <faulkner1993@hotmail.co.uk>
2022-12-13 12:25:16 +0000

    Update allowed_hosts in settings.py file

--- havscalcdb/settings.py
+++ havscalcdb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['havs-calc-db-project.herokapp.com', 'localhost']
+ALLOWED_HOSTS = ['havs-calc-db-project.herokuapp.com', 'localhost']
 
 
 # Application definition
